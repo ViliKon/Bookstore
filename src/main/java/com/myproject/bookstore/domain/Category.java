5451ec68fcdd54077f1bf082b674e6ec23007bf9
Vili Konttinen <vk23ik@gmail.com>
2024-02-28 11:41:59 +0200

    Adding Rest methods

--- src/main/java/com/myproject/bookstore/domain/Category.java
+++ src/main/java/com/myproject/bookstore/domain/Category.java
@@ -2,6 +2,8 @@ package com.myproject.bookstore.domain;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -16,7 +18,8 @@ public class Category {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long categoryid;
 	private String name;
-
+	
+	@JsonIgnore
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
 	private List<Book> books;
 
